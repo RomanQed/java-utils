e3e6eea09c433d80cc55c5174f6866bd339b3406
Roman Bakaldin <gbakaldin@gmail.com>
2022-01-02 01:18:31 +0300

    Improve AbstractLink

--- src/main/java/com/github/romanqed/jutils/structs/AbstractLink.java
+++ src/main/java/com/github/romanqed/jutils/structs/AbstractLink.java
@@ -1,14 +1,12 @@
 package com.github.romanqed.jutils.structs;
 
-import java.util.Objects;
-
 public abstract class AbstractLink<T extends Link> implements Link {
     private T tail;
 
     @Override
     @SuppressWarnings("unchecked")
     public <E extends Link> void attach(E tail) {
-        this.tail = (T) Objects.requireNonNull(tail);
+        this.tail = (T) tail;
     }
 
     @Override
--- src/main/java/com/github/romanqed/jutils/structs/ValueLink.java
+++ src/main/java/com/github/romanqed/jutils/structs/ValueLink.java
@@ -3,7 +3,7 @@ package com.github.romanqed.jutils.structs;
 public class ValueLink<E> extends AbstractLink<ValueLink<E>> {
     private E value;
 
-    public ValueLink(E initValue){
+    public ValueLink(E initValue) {
         this.value = initValue;
     }
 
