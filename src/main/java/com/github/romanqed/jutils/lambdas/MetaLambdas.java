3d03e93ea5d47167c5084c4a2330a8438969756d
RomanQed <gbakaldin@gmail.com>
2022-04-22 01:28:51 +0300

    Move common logic from MetaLambdas to MetaFactory

--- src/main/java/com/github/romanqed/jutils/lambdas/MetaLambdas.java
+++ src/main/java/com/github/romanqed/jutils/lambdas/MetaLambdas.java
@@ -1,6 +1,7 @@
 package com.github.romanqed.jutils.lambdas;
 
-import java.lang.invoke.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -10,92 +11,29 @@ import java.util.concurrent.Callable;
  * A set of methods that simplify interaction with the meta-lambda generator added in java 8.
  */
 public final class MetaLambdas {
-    private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
+    private static final MetaFactory FACTORY = new MetaFactory(MethodHandles.lookup());
     private static final LambdaClass<?> HANDLER = LambdaClass.fromClass(Handler.class);
     private static final LambdaClass<?> ACTION = LambdaClass.fromClass(Action.class);
     private static final LambdaClass<?> CALLABLE = LambdaClass.fromClass(Callable.class);
 
-    public static MethodType extractType(Method method) throws IllegalAccessException {
-        Objects.requireNonNull(method);
-        MethodHandle handle = LOOKUP.unreflect(method);
-        return handle.type();
-    }
-
-    public static MethodType extractDynamicType(Method method) throws IllegalAccessException {
-        return extractType(method).dropParameterTypes(0, 1);
-    }
-
-    public static Action<Object[], Object> packAnyMethod(Method method, Object bind, int arguments) throws Throwable {
-        Objects.requireNonNull(method);
-        method.setAccessible(true);
-        MethodHandle handle = LOOKUP.unreflect(method).asSpreader(Object[].class, arguments);
-        if (bind != null) {
-            handle = handle.bindTo(bind);
-        }
-        return handle::invoke;
-    }
-
-    public static Action<Object[], Object> packAnyMethod(Method method, int arguments) throws Throwable {
-        return packAnyMethod(method, null, arguments);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> T packLambdaHandle(LambdaClass<T> clazz, MethodHandle handle, Object bind) throws Throwable {
-        Objects.requireNonNull(clazz);
-        Method lambdaMethod = clazz.getLambdaMethod();
-        MethodType lambdaType = extractDynamicType(lambdaMethod);
-        MethodType bindType = MethodType.methodType(clazz.getLambdaClass());
-        MethodType sourceType = handle.type();
-        if (bind != null) {
-            bindType = bindType.appendParameterTypes(bind.getClass());
-            sourceType = sourceType.dropParameterTypes(0, 1);
-        }
-        CallSite callSite = LambdaMetafactory.metafactory(
-                LOOKUP,
-                lambdaMethod.getName(),
-                bindType,
-                lambdaType,
-                handle,
-                sourceType
-        );
-        MethodHandle ret = bind == null ? callSite.getTarget() : callSite.getTarget().bindTo(bind);
-        return (T) ret.invoke();
-    }
-
-    public static <T> T packLambdaMethod(LambdaClass<T> clazz, Method method, Object bind) throws Throwable {
-        Objects.requireNonNull(method);
-        MethodHandle handle = LOOKUP.unreflect(method);
-        return packLambdaHandle(clazz, handle, bind);
-    }
-
-    public static <T> T packLambdaConstructor(LambdaClass<T> clazz, Constructor<?> constructor) throws Throwable {
-        Objects.requireNonNull(constructor);
-        MethodHandle handle = LOOKUP.unreflectConstructor(constructor);
-        return packLambdaHandle(clazz, handle, null);
-    }
-
-    public static <T> T packLambdaMethod(LambdaClass<T> clazz, Method method) throws Throwable {
-        return packLambdaMethod(clazz, method, null);
-    }
-
     @SuppressWarnings("unchecked")
     public static <T> Handler<T> packHandler(Method method, Object bind) throws Throwable {
-        return (Handler<T>) packLambdaMethod(HANDLER, method, bind);
+        return (Handler<T>) FACTORY.packLambdaMethod(HANDLER, method, bind);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> Handler<T> packHandler(Method method) throws Throwable {
-        return (Handler<T>) packLambdaMethod(HANDLER, method, null);
+        return (Handler<T>) FACTORY.packLambdaMethod(HANDLER, method, null);
     }
 
     @SuppressWarnings("unchecked")
     public static <T, R> Action<T, R> packAction(Method method, Object bind) throws Throwable {
-        return (Action<T, R>) packLambdaMethod(ACTION, method, bind);
+        return (Action<T, R>) FACTORY.packLambdaMethod(ACTION, method, bind);
     }
 
     @SuppressWarnings("unchecked")
     public static <T, R> Action<T, R> packAction(Method method) throws Throwable {
-        return (Action<T, R>) packLambdaMethod(ACTION, method, null);
+        return (Action<T, R>) FACTORY.packLambdaMethod(ACTION, method, null);
     }
 
     @SuppressWarnings("unchecked")
@@ -103,25 +41,25 @@ public final class MetaLambdas {
         Objects.requireNonNull(clazz);
         Objects.requireNonNull(argumentClazz);
         Constructor<R> toPack = clazz.getDeclaredConstructor(argumentClazz);
-        MethodHandle handle = LOOKUP.unreflectConstructor(toPack);
-        return (Action<T, R>) packLambdaHandle(ACTION, handle, null);
+        MethodHandle handle = FACTORY.getLookup().unreflectConstructor(toPack);
+        return (Action<T, R>) FACTORY.packLambdaHandle(ACTION, handle, null);
     }
 
     @SuppressWarnings("unchecked")
     public static <R> Callable<R> packCallable(Method method, Object bind) throws Throwable {
-        return (Callable<R>) packLambdaMethod(CALLABLE, method, bind);
+        return (Callable<R>) FACTORY.packLambdaMethod(CALLABLE, method, bind);
     }
 
     @SuppressWarnings("unchecked")
     public static <R> Callable<R> packCallable(Method method) throws Throwable {
-        return (Callable<R>) packLambdaMethod(CALLABLE, method, null);
+        return (Callable<R>) FACTORY.packLambdaMethod(CALLABLE, method, null);
     }
 
     @SuppressWarnings("unchecked")
     public static <R> Callable<R> packConstructor(Class<R> clazz) throws Throwable {
         Objects.requireNonNull(clazz);
         Constructor<R> toPack = clazz.getDeclaredConstructor();
-        MethodHandle handle = LOOKUP.unreflectConstructor(toPack);
-        return (Callable<R>) packLambdaHandle(CALLABLE, handle, null);
+        MethodHandle handle = FACTORY.getLookup().unreflectConstructor(toPack);
+        return (Callable<R>) FACTORY.packLambdaHandle(CALLABLE, handle, null);
     }
 }
